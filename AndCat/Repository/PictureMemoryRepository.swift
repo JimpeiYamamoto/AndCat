c5e75825e90061abc927245dcbcc887027a940f8
Ueeek <uekikoichiro@gmail.com>
2024-10-05 17:16:12 +0900

    made repository async

--- AndCat/Repository/PictureMemoryRepository.swift
+++ AndCat/Repository/PictureMemoryRepository.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public protocol PictureMemoryRepositoryType {
-    func get(first: Date, last: Date) -> [PictureMemory]
-    func get(with category: Category) -> [PictureMemory]
-    func save(_ pictureMemory: PictureMemory)
+    func get(first: Date, last: Date) async -> [PictureMemory]
+    func get(with category: Category) async -> [PictureMemory]
+    func save(_ pictureMemory: PictureMemory) async
 }
