f1313b3870cdb393fbece035fdc9d71ecc2a8a16
JimpeiYamamoto <zinpei104@gmail.com>
2024-10-05 15:13:41 +0900

    Initial Commit

---
+++ AndCat/AndCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndCatApp.swift
+//  AndCat
+//
+//  Created by 山本 迅平 on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AndCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AndCat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AndCat
+//
+//  Created by 山本 迅平 on 10/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
