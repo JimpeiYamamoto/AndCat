b8da4656132b94d952f3b36a5de6f239320d11f3
yuyakan <kanbe1365@icloud.com>
2024-10-06 01:42:17 +0900

    カレンダービューを作成した

--- AndCat/View/CalendarView.swift
+++ AndCat/View/CalendarView.swift
@@ -8,22 +8,158 @@
 import SwiftUI
 
 struct CalendarView: View {
+    var calendar = Calendar.current
+
+    // 過去12か月分の月を取得
+    var pastYearMonths: [Date] {
+        var months = [Date]()
+        for i in 0..<12 {
+            if let month = calendar.date(byAdding: .month, value: -i, to: Date()) {
+                months.append(month)
+            }
+        }
+        return months
+    }
+    
     var body: some View {
-        BasicMultiDatePicker()
+        ZStack {
+            Color(red: 230, green: 234, blue: 237, opacity: 1)
+            ScrollView {
+                VStack(spacing: 10) {
+                    // 各月のカレンダーを表示
+                    ForEach(pastYearMonths.reversed(), id: \.self) { month in
+                        MonthlyCalendarView(selectedDate: month, viewStream: CalenderViewStream.shared)
+                    }
+                }
+                .padding(.horizontal, 10)
+            }
+        }
     }
 }
 
-#Preview {
-    CalendarView()
+struct MonthlyCalendarView<Stream: CalendarViewStreamType>: View {
+    @StateObject var viewStream: Stream
+    var selectedDate: Date
+    private var calendar = Calendar.current
+    
+    init(selectedDate: Date, viewStream: Stream) {
+        self.selectedDate = selectedDate
+        _viewStream = StateObject(wrappedValue: viewStream)
+    }
+    
+    var body: some View {
+        VStack {
+            // 月名を表示
+            HStack {
+                Text("\(monthName)")
+                    .font(.body)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 10)
+                Spacer()
+            }
+            
+            // 曜日名を表示
+            HStack {
+                ForEach(weekdays, id: \.self) { weekday in
+                    Text(weekday)
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            
+            // カレンダーの日付を表示
+            LazyVGrid(columns: Array(repeating: GridItem(), count: 7)) {
+                // 月の1日が始まる曜日まで空白セルを追加
+                ForEach(0..<startDayIndex, id: \.description) { _ in
+                    Text("")
+                }
+                
+                // 月の日数を表示
+                ForEach(daysInMonth, id: \.self) { day in
+                    ZStack {
+                        if viewStream.output.isPresentLoadingView {
+                            ProgressView()
+                                .frame(width: 50, height: 50)
+                        } else {
+                            if let dateDict,
+                               let pictureMemory = dateDict[day] {
+                                Image(uiImage: pictureMemory.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                
+                            }
+                            Text("\(day)")
+                                .frame(width: 50, height: 50)
+                        }
+                    }
+                }
+            }
+        }
+        .onAppear {
+            Task {
+                await viewStream.action(input: .onAppear)
+            }
+        }
+        .padding()
+    }
+    
+    // 曜日名（日曜日から）
+    var weekdays: [String] {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ja_JP")
+        return formatter.shortWeekdaySymbols
+    }
+
+    // 月名を取得
+    var monthName: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ja_JP")
+        formatter.dateFormat = "LLLL yyyy"
+        return formatter.string(from: selectedDate)
+    }
+    
+    // yyyyMMをIntで取得
+    var yearAndMonthInt: Int {
+        // 日付をフォーマット
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMM"
+        let yearAndMonthString = formatter.string(from: selectedDate)
+        return Int(yearAndMonthString)!
+    }
+    
+    // 日付のDictを取得
+    var dateDict: Dictionary<Int, CalenderViewStreamModel.PictureMemory>? {
+        if let dateDict = viewStream.output.pictureMemoryDict[yearAndMonthInt] {
+            return dateDict
+        }
+        return nil
+    }
+    
+   
+    // 月の1日が始まる曜日（0=日曜日, 1=月曜日, ...）
+    var startDayIndex: Int {
+        guard let firstDay = getDate(day: 1) else { return 0 }
+        let weekday = calendar.component(.weekday, from: firstDay)
+        return weekday - 1 // 日曜日を0、月曜日を1として計算
+    }
+
+    // 月の日数を取得
+    var daysInMonth: [Int] {
+        guard let range = calendar.range(of: .day, in: .month, for: selectedDate) else { return [] }
+        return Array(range)
+    }
+
+    // 特定の日の正確なDateを取得
+    func getDate(day: Int) -> Date? {
+        var components = calendar.dateComponents([.year, .month], from: selectedDate)
+        components.day = day
+        return calendar.date(from: components)
+    }
 }
 
-struct BasicMultiDatePicker: View {
-
-    // 選択日付を保持するプロパティ
-    @State private var dates: Set<DateComponents> = []
-
-    var body: some View {
-
-        MultiDatePicker("複数の日付選択", selection: $dates)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
     }
 }
---
+++ AndCat/ViewStream/CalendarViewStream.swift
@@ -0,0 +1,107 @@
+//
+//  CalendarViewStream.swift
+//  AndCat
+//
+//  Created by 上別縄祐也 on 2024/10/05.
+//
+
+import Foundation
+import UIKit
+
+public protocol CalendarViewStreamType: ViewStreamType
+where Output == CalenderViewStreamModel.Output,
+      Input == CalenderViewStreamModel.Input,
+      State == CalenderViewStreamModel.State
+{}
+
+public final class CalenderViewStream: CalendarViewStreamType {
+    public var state = CalenderViewStreamModel.State()
+    private let useCase: CalenderViewUseCaseType
+
+    @Published public var output = CalenderViewStreamModel.Output(
+        pictureMemoryDict: [:],
+        isPresentLoadingView: true
+    )
+    
+    public init(useCase: CalenderViewUseCaseType) {
+        self.useCase = useCase
+    }
+
+    @MainActor
+    public func action(
+        input: CalenderViewStreamModel.Input
+    ) async {
+        switch input {
+        case .onAppear:
+            output.isPresentLoadingView = true
+            defer {
+                output.isPresentLoadingView = false
+            }
+
+            // 非同期処理のみバックグランドスレッドで実行するように指定
+            let fetchResult = await Task.detached(priority: .background) {
+                await self.useCase.fetchPictureMemoryList(first: Calendar.current.date(byAdding: .month, value: -11, to: Date())!, last: Date())
+            }.value
+
+            switch fetchResult {
+            case let .success(pictureMemoryList):
+                var yearAndMonthDict: Dictionary<Int, Dictionary<Int, CalenderViewStreamModel.PictureMemory>> = [:]
+                for pictureMemory in pictureMemoryList {
+                    // 日付をIntに変換
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "yyyyMM"
+                    let yearAndMonthInt = Int(formatter.string(from: pictureMemory.date))!
+                    formatter.dateFormat = "dd"
+                    let dateInt = Int(formatter.string(from: pictureMemory.date))!
+                    
+                    let pictureMemory = CalenderViewStreamModel.PictureMemory(date: pictureMemory.date, image: pictureMemory.image, theme: pictureMemory.theme)
+                    if var dict = yearAndMonthDict[yearAndMonthInt] {
+                        dict.updateValue(pictureMemory, forKey: dateInt)
+                        yearAndMonthDict.updateValue(dict, forKey: yearAndMonthInt)
+                    } else {
+                        let dict = [dateInt: pictureMemory]
+                        yearAndMonthDict.updateValue(dict, forKey: yearAndMonthInt)
+                    }
+                }
+                output.pictureMemoryDict = yearAndMonthDict
+            }
+        }
+    }
+}
+
+public enum CalenderViewStreamModel {
+    // viewからの得られるイベントを管理するenum
+    public enum Input {
+        case onAppear
+    }
+
+    // Viewへ描画する値を管理するStruct
+    public struct Output {
+        public var pictureMemoryDict: Dictionary<Int,Dictionary<Int, PictureMemory>>
+        public var isPresentLoadingView: Bool
+
+        public init(
+            pictureMemoryDict: Dictionary<Int,Dictionary<Int, PictureMemory>>,
+            isPresentLoadingView: Bool
+        ) {
+            self.pictureMemoryDict = pictureMemoryDict
+            self.isPresentLoadingView = isPresentLoadingView
+        }
+    }
+    
+    public struct State {
+        
+    }
+}
+
+extension CalenderViewStreamModel {
+    public struct PictureMemory {
+        let date: Date
+        let image: UIImage
+        let theme: Theme
+    }
+}
+
+extension CalenderViewStream {
+    public static let shared = CalenderViewStream(useCase: CalenderViewUseCase.shared)
+}
