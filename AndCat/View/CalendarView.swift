f4de06bfb4ac1cddc0501c6557b96dec8e1470b6
yuyakan <kanbe1365@icloud.com>
2024-10-05 17:18:25 +0900

    テスト

--- AndCat/View/CalendarView.swift
+++ AndCat/View/CalendarView.swift
@@ -9,10 +9,21 @@ import SwiftUI
 
 struct CalendarView: View {
     var body: some View {
-        Text("Calendar")
+        BasicMultiDatePicker()
     }
 }
 
 #Preview {
     CalendarView()
+}
+
+struct BasicMultiDatePicker: View {
+
+    // 選択日付を保持するプロパティ
+    @State private var dates: Set<DateComponents> = []
+
+    var body: some View {
+
+        MultiDatePicker("複数の日付選択", selection: $dates)
+    }
 }
