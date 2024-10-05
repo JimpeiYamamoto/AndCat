9d023f898b784e97dc202d894653210644335540
Ueeek <uekikoichiro@gmail.com>
2024-10-06 03:54:45 +0900

    fix conflict

--- AndCat/AndCatApp.swift
+++ AndCat/AndCatApp.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import UserNotifications
+import FacebookCore
 
 @main
 struct AndCatApp: App {
@@ -14,11 +15,29 @@ struct AndCatApp: App {
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     // PushNotificationを受信するためのPermissionを求める。UserがOKすれば受け取れる。
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, _) in
-            if granted {
-                UNUserNotificationCenter.current().delegate = self
-            }
-        }
+//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, _) in
+//            if granted {
+//                UNUserNotificationCenter.current().delegate = self
+//            }
+//        }
+        
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
         return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any]
+    ) -> Bool {
+        ApplicationDelegate.shared.application(
+            application,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
     }
 }
